05de3a8c38abe1dc39a4b5b2a326b4c782d582fb
joeriddles <joeriddles10@gmail.com>
2024-07-03 11:34:45 -0700

    Make integration test do something interesting

--- app/integration_test/main_test.dart
+++ app/integration_test/main_test.dart
@@ -1,15 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
+import 'package:intellicrews/camera.dart';
+import 'package:intellicrews/main.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   testWidgets("turn on camera", (WidgetTester tester) async {
     // Arrange
+    await tester.pumpWidget(const MyApp());
 
     // Act
+    final fabButton = find.byType(FloatingActionButton);
+    expect(fabButton, findsOneWidget);
+    await tester.tap(fabButton);
+    await tester.pump();
 
     // Assert
-    expect(true, false, reason: "Simple failing test");
+    final camera = find.byType(Camera);
+    expect(camera, findsOneWidget);
   });
 }
