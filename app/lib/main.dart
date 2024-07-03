8f3ac1e9727a4861c0650f039e10b1768348f8ae
joeriddles <joeriddles10@gmail.com>
2024-07-03 08:59:02 -0700

    Add button to toggle camera and handle camera loading errors

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,59 +1,86 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:intellicrews/camera.dart';
 
-late List<CameraDescription> _cameras;
-
-Future<void> main() async {
-  _cameras = await availableCameras();
-  runApp(const CameraApp());
+main() {
+  runApp(const MyApp());
 }
 
-class CameraApp extends StatefulWidget {
-  const CameraApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<CameraApp> createState() => _CameraAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IntelliCrews',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'IntelliCrews'),
+    );
+  }
 }
 
-class _CameraAppState extends State<CameraApp> {
-  late CameraController controller;
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
 
   @override
-  void initState() {
-    super.initState();
-    controller = CameraController(_cameras[0], ResolutionPreset.max);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool _isCameraOn = false;
+  String? _cameraError;
+  List<CameraDescription> _cameras = [];
+
+  Future<void> _onCameraToggled() async {
+    final isCameraOn = !_isCameraOn;
+    if (isCameraOn && _cameras.isEmpty) {
+      try {
+        final cameras = await availableCameras();
+        setState(() {
+          _cameras = cameras;
+          _cameraError = null;
+        });
+      } on CameraException catch (error) {
+        setState(() {
+          _cameraError = error.description;
+        });
       }
-      setState(() {});
-    }).catchError((Object e) {
-      if (e is CameraException) {
-        switch (e.code) {
-          case 'CameraAccessDenied':
-            // Handle access errors here.
-            break;
-          default:
-            // Handle other errors here.
-            break;
-        }
-      }
+    }
+    setState(() {
+      _isCameraOn = isCameraOn;
     });
   }
 
   @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return Container();
-    }
-    return MaterialApp(
-      home: CameraPreview(controller),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_cameraError != null)
+              Text(_cameraError!)
+            else if (_isCameraOn)
+              Camera(cameras: _cameras),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onCameraToggled,
+        tooltip: _isCameraOn ? 'Turn off camera' : 'Turn on camera',
+        child: _isCameraOn
+            ? const Icon(Icons.toggle_on)
+            : const Icon(Icons.toggle_off_outlined),
+      ),
     );
   }
 }
