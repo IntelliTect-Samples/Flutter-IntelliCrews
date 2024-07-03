5dc921c4fbbad1d118a97df2945456985132f8bc
joeriddles <joeriddles10@gmail.com>
2024-07-03 10:31:52 -0700

    Add simple failing integration test

---
+++ app/integration_test/main_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets("turn on camera", (WidgetTester tester) async {
+    // Arrange
+
+    // Act
+
+    // Assert
+    expect(true, false, reason: "Simple failing test");
+  });
+}
